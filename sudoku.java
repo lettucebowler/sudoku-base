78c1ab8f86852e77374894b36513a11da8a815d7
Grant Montgomery <montg433@d.umn.edu>
2020-06-09 13:47:29 -0500

    toString() works now

--- sudoku.java
+++ sudoku.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Sudoku {
     private int board_size;
     private int cell_size;
@@ -46,52 +49,60 @@ class Sudoku {
     public String to_string() {
         int width = this.board_size;
         StringBuilder builder = new StringBuilder();
-        String div = this.divider();
+        String bar = divider();
         for (int i = 0; i < this.cell_size; i++) {
-            builder.append(div);
+            builder.append(bar);
             builder.append("\n");
             for (int j = 0; j < this.cell_size; j++) {
-                builder.append(row(i * this.cell_size + j));
+                builder.append(this.row(i * this.cell_size + j));
             }
         }
-        builder.append(div);
+        builder.append(bar);
         return builder.toString();
     }
 
     private String divider() {
-        StringBuilder div_builder = new StringBuilder();
-        div_builder.append("+");
+        StringBuilder bar_builder = new StringBuilder();
+        int number_width = (int) Math.ceil(Math.log10(this.board_size));
+
+        bar_builder.append("+");
         for(int i = 0; i < this.cell_size; i++) {
-            for(int j = 0; j < this.cell_size * 2 + 1; j++) {
-                div_builder.append("-");
+            for(int j = 0; j < this.cell_size * (number_width + 1) + 1; j++) {
+                bar_builder.append("-");
             }
-            div_builder.append("+");
+            bar_builder.append("+");
         }
-        // builder.append("\n");
-        return div_builder.toString();
+        return bar_builder.toString();
     }
 
     private String row(int row_index) {
         StringBuilder row_builder = new StringBuilder();
-        // for (int i = 0; i < this.board_size; i++) {
-            // row_builder.append(this.board_filled[row_index][i]);
-            row_builder.append("| ");
-            for(int i = 0; i < this.cell_size; i++) {
-                for(int j = 0; j < this.cell_size; j++) {
-                    row_builder.append(board_filled[row_index][j + (i * this.cell_size)]);
+        int max_digits = (this.board_size * this.board_size);
+        String bound = Integer.toString(max_digits);
+        max_digits = bound.length();
+
+        row_builder.append("| ");
+        for(int i = 0; i < this.cell_size; i++) {
+            for(int j = 0; j < this.cell_size; j++) {
+                int num = this.board_filled[row_index][i + j];
+                int num_digits = Integer.toString(num).length();
+                int needed_digits = max_digits - num_digits;
+                for (int k = 1; k < needed_digits; k++) {
                     row_builder.append(" ");
                 }
-                row_builder.append("| ");
+                row_builder.append(num);
+                row_builder.append(" ");
             }
-            row_builder.append("\n");
+            row_builder.append("| ");
+        }
+        row_builder.append("\n");
         return row_builder.toString();
     }
 
     public static void main(String args[]) {
         int cell_size = 3;
-        Sudoku sudoku = new Sudoku(3);
+        Sudoku sudoku = new Sudoku(cell_size);
         sudoku.generate_filled();
-        // System.out.println(sudoku.get_board_filled().toString());
         System.out.println(sudoku.to_string());
 
     }
